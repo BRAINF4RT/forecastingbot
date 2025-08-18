0a2e971e01e6068a15c05c383b3e0055eb6de79a
BRAINF4RT <haphazardly.inc@tutanota.com>
2025-08-18 11:59:19 +1000

    Update main.py

--- main.py
+++ main.py
@@ -380,7 +380,7 @@ if __name__ == "__main__":
      # The Metaculus cup may not be initialized near the beginning of a season (i.e. January, May, September)
         forecast_reports = asyncio.run(
             template_bot.forecast_on_tournament(
-                MetaculusApi.MP25Q3_TOURNAMENT_ID, return_exceptions=True 
+                MP25Q3_TOURNAMENT_ID, return_exceptions=True
             )
         )       
     elif run_mode == "test_questions":
