f9a1e01f8c3cfbfa942069a043f656fb5afdbf58
BRAINF4RT <haphazardly.inc@tutanota.com>
2025-08-19 14:05:09 +1000

    Update main.py

--- main.py
+++ main.py
@@ -348,7 +348,7 @@ if __name__ == "__main__":
                  allowed_tries=2,
              ),
              "summarizer": "openrouter/meta-llama/llama-4-scout",
-             "researcher": "metaculus/anthropic/claude-3-5-sonnet-20241022:online",
+             "researcher": "openrouter/deepseek/deepseek-chat-v3-0324:free:online",
              "parser": "openrouter/meta-llama/llama-4-scout",
          },
     )         
