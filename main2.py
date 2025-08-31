74301906607f629366f8fd74d9ab724e5262c8ae
BRAINF4RT <haphazardly.inc@tutanota.com>
2025-08-31 14:27:07 +1000

    Update main2.py

--- main2.py
+++ main2.py
@@ -143,6 +143,7 @@ class FallTemplateBot2025(ForecastBot):
                    - Identify how often similar events have occurred in the past.
                    - Highlight similarities and differences between past cases and the present one.
                 Try to diversify your sources, but also ensure that they are reputable.
+                Information gathered from community driven, informal social media platforms should be marginalized as they are more likely to hold misinformation.
                 Tell the forecaster what YOU think the question will resolve as and why, however you do not produce forecasts yourself.
                 Your output prioritises quality information and it can be as large as it needs to be, as long as it gets all the relevent information across.
 
